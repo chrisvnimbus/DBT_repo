9641919f0b106a89c884eca3b3d9c73122f00eed
chris.verweij@nimbusintelligence.com <chris.verweij@nimbusintelligence.com>
2023-10-30 15:24:47 +0000

    freshness

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,14 +1,12 @@
-with payments as (
+
     
     select 
         id as payment_id,
         orderid as order_id,
-        
+
         amount
 
-    from raw.stripe.payment
+    from {{ source('stripe', 'payment') }}
 
     where status = 'success'
-)
 
-select * from payments
